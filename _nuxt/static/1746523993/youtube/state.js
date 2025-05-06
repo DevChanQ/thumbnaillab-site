2c11bcbfe3c3c3d41478fe5a6ca3fe39ea0002a1
DevJeff <45824907+DevChanQ@users.noreply.github.com>
2025-05-06 17:33:34 +0800

    35e11bc

---
+++ _nuxt/static/1746523993/youtube/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fthumbnaillab-site\u002F_nuxt\u002Fstatic\u002F1746523993",layout:"default",error:a,serverRendered:true,routePath:"\u002Fyoutube",config:{_app:{basePath:"\u002Fthumbnaillab-site\u002F",assetsPath:"\u002Fthumbnaillab-site\u002F_nuxt\u002F",cdnURL:a}}}}(null));

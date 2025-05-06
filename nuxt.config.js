35e11bc696306be1536d451ecec10cf1a111ee61
DevJeff <45824907+DevChanQ@users.noreply.github.com>
2025-05-06 17:26:46 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/thumbnaillab-site/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'SVG Editor - Graphic Design with SVG on Android/iOS',
